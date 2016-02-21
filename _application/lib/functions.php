cada835f54aebb3cefbae56b088dbc23415046ca
jsavell <contact@shadowater.com>
2016-02-21 12:48:52 -0600

    remove legacy autoloader

--- _application/lib/functions.php
+++ _application/lib/functions.php
@@ -1,23 +1,5 @@
 <?php
-/*
-spl_autoload_register(function ($name) {
-	$classLocations = array(
-							array("path"=>"{$GLOBALS['config']['path_classes']}viewrenderers/","suffix"=>"class"),
-							array("path"=>"{$GLOBALS['config']['path_interfaces']}","suffix"=>"interface"),
-							array("path"=>"{$GLOBALS['config']['path_lib']}","suffix"=>"class"));
-	$fileName = null;
-	foreach ($classLocations as $location) {
-		$fileName = "{$location['path']}{$name}.{$location['suffix']}.php";
-		if (is_file($fileName)) {
-			break;
-		}
-		$fileName = null;
-	}
-	if ($fileName) {
-		require $fileName;
-	}
-});
-*/
+
 spl_autoload_register(function ($class) {
     // project-specific namespace prefix
     $prefix = 'TAMU\\Seed\\';
