f475253b8602c7f48369eee5cf019352737d4061
jsavell <contact@shadowater.com>
2016-02-21 19:43:35 -0600

    get started on documentation

--- _application/TAMU/Seed/Interfaces/ViewRenderer.php
+++ _application/TAMU/Seed/Interfaces/ViewRenderer.php
@@ -1,5 +1,13 @@
 <?php
 namespace TAMU\Seed\Interfaces;
+/** 
+*	An interface defining a ViewRenderer
+*	ViewRenderers are utilized by controllers and handle the presentation of data to the user
+*	Built in implementations are HTMLViewRenderer and JSONViewRenderer
+*
+*	@author Jason Savell <jsavell@library.tamu.edu>
+*/
+
 interface ViewRenderer {
 	public function renderView();
 	public function setViewVariables($data);
