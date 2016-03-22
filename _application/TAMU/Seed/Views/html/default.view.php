567d2388a3a04812edeec7dcad3331594f2c766f
jsavell <jsavell@library.tamu.edu>
2016-03-22 15:33:42 -0500

    improve default view

--- _application/TAMU/Seed/Views/html/default.view.php
+++ _application/TAMU/Seed/Views/html/default.view.php
@@ -1,1 +1,10 @@
-<div>This is the default view.</div>
+<div>
+	This is the default view.
+<?php
+if ($globaluser->isLoggedIn()) {
+	echo " Welcome, {$globaluser->getProfileValue('username')}.";
+} else {
+	echo " You can log in, <a href=\"{$app_http}login.php\">here</a>.";
+}
+?>
+</div>
