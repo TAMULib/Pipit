bd3888f72ca792ae68352bddc6bd69d3777b5af7
jsavell <jsavell@library.tamu.edu>
2016-03-23 15:55:45 -0500

    update sql schema

--- install/phpseedapp.sql
+++ install/phpseedapp.sql
@@ -1,70 +1,59 @@
--- phpMyAdmin SQL Dump
--- version 4.0.10deb1
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Feb 17, 2015 at 10:40 PM
--- Server version: 5.5.40-0ubuntu0.14.04.1
--- PHP Version: 5.5.9-1ubuntu4.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
 --
--- Database: `phpseedapp`
+-- Database: 'phpseedapp'
 --
+CREATE DATABASE IF NOT EXISTS phpseedapp DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE phpseedapp;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Table structure for table 'users'
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(20) NOT NULL,
+CREATE TABLE IF NOT EXISTS users (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  username varchar(20) NOT NULL,
   `password` varchar(80) NOT NULL,
-  `email` varchar(120) NOT NULL,
-  `name_first` varchar(30) NOT NULL,
-  `name_last` varchar(30) NOT NULL,
-  `isadmin` tinyint(1) NOT NULL,
-  `inactive` tinyint(1) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
+  email varchar(120) NOT NULL,
+  name_first varchar(30) NOT NULL,
+  name_last varchar(30) NOT NULL,
+  isadmin tinyint(1) NOT NULL DEFAULT '0',
+  inactive tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
+-- Default password is 'changethis' --
+INSERT INTO `users` (`username`, `password`, `email`, `name_first`, `name_last`, `isadmin`, `inactive`) VALUES
+('admin', '$2y$10$XWsCnoBSNE2P6YKD3ERqZ.Wjwtq1RR5fgXKVcRYaWtmpkPGbYyi.G', '', 'Adam', 'Admin', 1, 0);
+
+
 --
--- Table structure for table `users_ldap`
+-- Table structure for table 'users_ldap'
 --
 
-CREATE TABLE IF NOT EXISTS `users_ldap` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `userid` int(11) NOT NULL,
-  `samaccountname` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `userid` (`userid`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
+CREATE TABLE IF NOT EXISTS users_ldap (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  userid int(11) NOT NULL,
+  samaccountname varchar(50) NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY userid (userid)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `widgets`
+-- Table structure for table 'widgets'
 --
 
-CREATE TABLE IF NOT EXISTS `widgets` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS widgets (
+  id int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
-  `description` text NOT NULL,
-  PRIMARY KEY (`id`)
+  description text NOT NULL,
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
