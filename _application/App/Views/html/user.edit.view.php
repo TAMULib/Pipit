6cb7909d0be3da2bd0e69d2c844e5c833c271737
jsavell <jsavell@library.tamu.edu>
2016-03-24 10:11:02 -0500

    user resetable password

--- _application/App/Views/html/user.edit.view.php
+++ _application/App/Views/html/user.edit.view.php
@@ -1,8 +1,8 @@
 <?php
 $user = $parameters['user'];
-echo '<form class="do-submit" name="updateuser" method="POST" action="'.$app_http.'">
+echo '<form class="do-submit column column-half" name="updateuser" method="POST" action="'.$app_http.'">
 			<input type="hidden" name="action" value="update" />
-			<div class="column column-half">
+			<div>
 				<label for="user[name_last]">Last Name</label>
 				<input type="text" name="user[name_last]" value="'.$user['name_last'].'" />
 				<label for="user[name_first]">First Name</label>
@@ -11,5 +11,15 @@ echo '<form class="do-submit" name="updateuser" method="POST" action="'.$app_htt
 				<input type="text" name="user[email]" value="'.$user['email'].'" />
 			</div>
 			<input type="submit" name="submituser" value="Update User" />
+		</form>';
+echo '<form class="do-submit-reset column column-half" name="updateuserpassword" method="POST" action="'.$app_http.'">
+			<input type="hidden" name="action" value="update" />
+			<div>
+				<label for="user[password]">Password</label>
+				<input id="newPassword" type="password" name="user[password]" />
+				<label for="confirmpassword">Confirm Password</label>
+				<input id="confirmNewPassword" type="password" name="confirmpassword" />
+			</div>
+			<input id="submitPasswordReset" type="submit" name="submitpassword" value="Reset Password" disabled />
 		</form>';
 ?>
