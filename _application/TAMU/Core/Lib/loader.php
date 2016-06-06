878eae260d541c44d6a2213a93be79ff12804464
jsavell <jsavell@library.tamu.edu>
2016-06-06 15:34:33 -0500

    (wip) convert to class based loader

--- _application/TAMU/Core/Lib/loader.php
+++ _application/TAMU/Core/Lib/loader.php
@@ -1,5 +1,6 @@
 <?php
 namespace TAMU\Core;
+use App\Classes as AppClasses;
 
 /**
 *	The entry point for the application. All endpoints lead, here.
@@ -11,8 +12,18 @@ namespace TAMU\Core;
 
 session_start();
 
-//load the constants
+require_once "{$config['PATH_LIB']}functions.php";
+
+//This array represents the app's pages. Used to generate user facing navigation and load controllers
+//The keys represent controller names
+$pages = array(
+			"widgets" => array("name"=>"widgets","path"=>"widgets"),
+			"users" => array("name"=>"users","path"=>"users","admin"=>true));
+
 $config = get_defined_constants(true)["user"];
+//$site = new AppClasses\Site($globaluser,$config,$viewRenderer,$pages);
+$site = new AppClasses\Site($config,$pages);
+
 
 //don't recommend using, sanitizing in case someone does
 $_SERVER['PHP_SELF'] = htmlentities($_SERVER['PHP_SELF']);
@@ -23,89 +34,31 @@ if (isset($forceRedirectUrl) && !empty($forceRedirectUrl)) {
 
 $system = array();
 
-require_once "{$config['PATH_LIB']}functions.php";
-
-//This array represents the app's pages. Used to generate user facing navigation and load controllers
-//The keys represent controller names
-$pages = array(
-			"widgets" => array("name"=>"widgets","path"=>"widgets"),
-			"users" => array("name"=>"users","path"=>"users","admin"=>true));
-
-
-// $data is a wrapper for incoming data
-//do not access $_POST, $_GET, $_REQUEST directly
-if (!isset($data)) {
-	if (!empty($_GET['action'])) {
-		//restrict any controller actions that alter DB data to POST
-		$restrictedActions = array("insert","remove","update");
-		if (!in_array($_GET['action'],$restrictedActions)) {
-			$data = $_GET;
-		}
-	} elseif (!empty($_POST['action'])) {
-		$data = $_POST;
-	} else {
-		$data = $_REQUEST;
-	}
-}
-
-//get the user
-if (isset($config['USECAS']) && $config['USECAS']) {
-	$globaluser = new Classes\Data\UserCAS();
-	if (!empty($_GET['ticket'])) {
-		if ($globaluser->processLogIn($_GET['ticket'])) {
-			header("Location:{$config['PATH_HTTP']}");
-		}
-	} elseif (!$globaluser->isLoggedIn() && !isset($data['action'])) {
-		$globaluser->initiateLogIn();
-	}
-} else {
-	$globaluser = new Classes\Data\User();
-}
+//get a sanitized version of GET/POST/REQUEST data
+$data = $site->getInputData();
 
 //set the ViewRenderer
 if (isset($data['json']) && $data['json']) {
-	$viewRenderer = new Classes\ViewRenderers\JSONViewRenderer();
+	$site->setViewRenderer(new Classes\ViewRenderers\JSONViewRenderer());
 } else {
-	$viewRenderer = new Classes\ViewRenderers\HTMLViewRenderer();
+	$site->setViewRenderer(new Classes\ViewRenderers\HTMLViewRenderer());
 }
 
-//load admin controller if user is logged in and an admin page
-if (array_key_exists($controller,$pages) || $controller == 'user') {
-	if (!empty($pages[$controller]['admin']) && $pages[$controller]['admin'] == true) {
-		//if the user is an admin, load the admin controller, otherwise, redirect to the home page
-		if ($globaluser->isAdmin()) {
-			if ($controller) {
-				$viewRenderer->registerAppContextProperty("app_http", "{$config['PATH_HTTP']}admin/{$controller}/");
-				$filename = "{$config['PATH_CONTROLLERS']}admin/{$controller}.control.php";
-			} else {
-				$viewRenderer->registerAppContextProperty("app_http", "{$config['PATH_HTTP']}admin/");
-				$filename = "{$config['PATH_CONTROLLERS']}admin/default.control.php";
-			}
-		} else {
-			header("Location:{$config['PATH_HTTP']}");
-		}
-	} elseif ($globaluser->isLoggedIn() || (!$globaluser->isLoggedIn() && $controller == 'user')) {
-		//load standard controller
-		$viewRenderer->registerAppContextProperty("app_http", "{$config['PATH_HTTP']}{$controller}/");
 
-		$filename = "{$config['PATH_CONTROLLERS']}{$controller}.control.php";
-	} else {
-		header("Location:{$config['PATH_HTTP']}");
-	}
-} else {
-	$viewRenderer->registerAppContextProperty("app_http", "{$config['PATH_HTTP']}");
-	$filename = "{$config['PATH_CONTROLLERS']}default.control.php";
+$controllerPath = $site->getControllerPath($controller);
+if (!$controllerPath) {
+	header("Location:{$config['PATH_HTTP']}");
 }
 
 //try to load the controller
-if (!empty($filename) && is_file($filename)) {
-	include $filename;
+if (!empty($controllerPath) && is_file($controllerPath)) {
+	include $controllerPath;
 	//if the controller defined a $viewfile, register it with the view renderer
 	if (isset($viewName)) {
 		if (!empty($pages[$controller]['admin']) && $pages[$controller]['admin'] == true) {
-			$viewRenderer->setView($viewName,$globaluser->isAdmin());
+			$site->getViewRenderer()->setView($viewName,$globaluser->isAdmin());
 		} else {
-			$viewRenderer->setView($viewName);
+			$site->getViewRenderer()->setView($viewName);
 		}
 	}
 } else {
@@ -113,8 +66,8 @@ if (!empty($filename) && is_file($filename)) {
 }
 
 //send system messages to the ViewRenderer
-$viewRenderer->registerAppContextProperty("system", $system);
+$site->getViewRenderer()->registerAppContextProperty("system", $system);
 
 //display the content
-$viewRenderer->renderView();
+$site->getViewRenderer()->renderView();
 ?>
