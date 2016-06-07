d7fab8f4954f5da08abd4a7c4092b26e236e44d9
jsavell <jsavell@library.tamu.edu>
2016-06-07 15:19:53 -0500

    document functions files

--- _application/App/Lib/functions.php
+++ _application/App/Lib/functions.php
@@ -1,3 +1,9 @@
 <?php
+/**
+*	Any global functions should be placed, here.
+*	The core functions included provide an autoloader that handles the including of Classes and Interfaces
+*
+*/
+
 include PATH_CORE_LIB.'functions.php';
 ?>
