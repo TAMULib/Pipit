d7494d5110a60956d6224ea85a72fff92fb4b4b4
jsavell <jsavell@library.tamu.edu>
2016-06-08 14:31:31 -0500

    convert widget forms to bootstrap

--- _application/App/Views/html/widgets.add.view.php
+++ _application/App/Views/html/widgets.add.view.php
@@ -1,12 +1,14 @@
 <?php
 echo '<form class="do-submit" name="addbuilding" method="POST" action="'.$app_http.'">
 			<input type="hidden" name="action" value="insert" />
-			<div class="column column-half">
+			<div class="form-group">
 				<label for="widget[name]">Name</label>
-				<input type="text" name="widget[name]" />
-				<label for="widget[description]">Description</label>
-				<textarea name="widget[description]"></textarea>
+				<input class="form-control" type="text" name="widget[name]" />
 			</div>
-			<input type="submit" name="submitwidget" value="Add Widget" />
+			<div class="form-group">
+				<label for="widget[description]">Description</label>
+				<textarea class="form-control" name="widget[description]"></textarea>
+			</div>
+			<input class="btn btn-default" type="submit" name="submitwidget" value="Add Widget" />
 		</form>';
 ?>
