03e899fd4b88152257b7bc25258d57efcda5b4df
jsavell <jsavell@library.tamu.edu>
2016-06-08 14:45:44 -0500

    convert user views to bootstrap

--- _application/App/Views/html/admin/users.add.view.php
+++ _application/App/Views/html/admin/users.add.view.php
@@ -1,22 +1,34 @@
 <?php
 echo '<form class="do-submit" name="adduser" method="POST" action="'.$app_http.'">
 			<input type="hidden" name="action" value="insert" />
-			<div class="column column-half">
-				<label for="user[name_first]">First Name</label>
-				<input type="text" name="user[name_first]" />
-				<label for="user[name_last]">Last Name</label>
-				<input type="text" name="user[name_last]" />
-				<label for="user[email]">Email</label>
-				<input type="text" name="user[email]" />
+			<div class="col col-sm-6">
+				<div class="form-group">
+					<label for="user[name_first]">First Name</label>
+					<input class="form-control" type="text" name="user[name_first]" />
+				</div>
+				<div class="form-group">
+					<label for="user[name_last]">Last Name</label>
+					<input class="form-control" type="text" name="user[name_last]" />
+				</div>
+				<div class="form-group">
+					<label for="user[email]">Email</label>
+					<input class="form-control" type="text" name="user[email]" />
+				</div>
 			</div>
-			<div class="column column-half">
-				<label for="user[username]">Username</label>
-				<input type="text" name="user[username]" />
-				<label for="user[password]">Password</label>
-				<input type="password" name="user[password]" />
-				<label for="confirmpassword">Confirm Password</label>
-				<input type="password" name="confirmpassword" />
+			<div class="col col-sm-6">
+				<div class="form-group">
+					<label for="user[username]">Username</label>
+					<input class="form-control" type="text" name="user[username]" />
+				</div>
+				<div class="form-group">
+					<label for="user[password]">Password</label>
+					<input class="form-control" type="password" name="user[password]" />
+				</div>
+				<div class="form-group">
+					<label for="confirmpassword">Confirm Password</label>
+					<input class="form-control" type="password" name="confirmpassword" />
+				</div>
 			</div>
-			<input type="submit" name="submituser" value="Add User" />
+			<input class="btn btn-default" type="submit" name="submituser" value="Add User" />
 		</form>';
 ?>
--- _application/App/Views/html/admin/users.edit.view.php
+++ _application/App/Views/html/admin/users.edit.view.php
@@ -3,14 +3,18 @@ $user = $parameters['user'];
 echo '<form class="do-submit" name="updateuser" method="POST" action="'.$app_http.'">
 			<input type="hidden" name="action" value="update" />
 			<input type="hidden" name="id" value="'.$user['id'].'" />
-			<div class="column column-half">
+			<div class="form-group">
 				<label for="user[name_last]">Last Name</label>
-				<input type="text" name="user[name_last]" value="'.$user['name_last'].'" />
-				<label for="user[name_first]">First Name</label>
-				<input type="text" name="user[name_first]" value="'.$user['name_first'].'" />
-				<label for="user[email]">Email</label>
-				<input type="text" name="user[email]" value="'.$user['email'].'" />
+				<input class="form-control" type="text" name="user[name_last]" value="'.$user['name_last'].'" />
 			</div>
-			<input type="submit" name="submituser" value="Update User" />
+			<div class="form-group">
+				<label for="user[name_first]">First Name</label>
+				<input class="form-control" type="text" name="user[name_first]" value="'.$user['name_first'].'" />
+			</div>
+			<div class="form-group">
+				<label for="user[email]">Email</label>
+				<input class="form-control" type="text" name="user[email]" value="'.$user['email'].'" />
+			</div>
+			<input class="btn btn-default" type="submit" name="submituser" value="Update User" />
 		</form>';
 ?>
