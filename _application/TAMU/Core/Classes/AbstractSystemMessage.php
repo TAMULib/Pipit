16b13c8e2802197cbac9dfcd1f748d7d9d3dc848
jsavell <jsavell@library.tamu.edu>
2016-06-09 11:33:41 -0500

    re-implement system messages

---
+++ _application/TAMU/Core/Classes/AbstractSystemMessage.php
@@ -0,0 +1,36 @@
+<?php
+namespace TAMU\Core\Classes;
+use TAMU\Core\Interfaces as Interfaces;
+
+/** 
+*	An abstract implementation of the SystemMessage interface
+*
+*	@author Jason Savell <jsavell@library.tamu.edu>
+*/
+
+class AbstractSystemMessage implements Interfaces\SystemMessage {
+	private $messageType;
+	private $message;
+
+	public function __construct($message,$messageType) {
+		$this->setMessage($message);
+		$this->setType($messageType);
+	}
+
+	public function setType($messageType) {
+		$this->messageType = $messageType;
+	}
+
+	public function getType() {
+		return $this->messageType;
+	}
+
+	public function setMessage($message) {
+		$this->message = $message;
+	}
+
+	public function getMessage() {
+		return $this->message;
+	}
+}
+?>
