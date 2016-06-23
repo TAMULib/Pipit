0b6fef6b0f1e08f42bf83cc4d409d921036f74d7
jsavell <jsavell@library.tamu.edu>
2016-06-23 11:02:57 -0500

    improve config_sample

--- App/Config/config_sample.php
+++ App/Config/config_sample.php
@@ -1,7 +1,16 @@
 <?php
+
+
+//This must be set to the real file path to the base directory of your server, e.g. '/var/www/html/'
+define('PATH_ROOT', '/'); 
+
 define('APP_NAME', 'The Seed App'); 
 define('APP_DIRECTORY', 'PHPSeedApp'); 
-define('APP_BASE', '_application/');
+
+
+//Optionally change this to your domain or IP.
+//Add port if non-standard
+define('PATH_HTTP', "http://localhost/".APP_DIRECTORY."/site/");
 
 define("SESSION_SCOPE",APP_DIRECTORY);
 
@@ -12,28 +21,29 @@ define("NAMESPACE_CORE","Core\\");
 define("NAMESPACE_APP","App\\");
 
 //server paths
-define('PATH_ROOT', '/'); 
+//These don't need to be touched unless you're changing the location of the respective directories
 define('PATH_FILE', PATH_ROOT.APP_DIRECTORY.'/');
-define('PATH_APP', PATH_FILE.APP_BASE);
+define('PATH_APP', PATH_FILE.'/');
 define('PATH_LIB', PATH_APP.str_replace('\\', '/', NAMESPACE_APP)."Lib/");
 define('PATH_CORE_LIB', PATH_APP.str_replace('\\', '/', NAMESPACE_CORE)."Lib/");
 define('PATH_CONTROLLERS', PATH_APP.str_replace('\\', '/', NAMESPACE_APP)."Controllers/");
 define('PATH_VIEWS', PATH_APP.str_replace('\\', '/', NAMESPACE_APP)."Views/");
 
 //web paths
-define('PATH_HTTP', "http://localhost/".APP_DIRECTORY."/");
+//These don't need to be touched unless you want to put your site assets somewhere else
 define('PATH_RESOURCES', PATH_HTTP."resources/");
-define('PATH_THEMES', PATH_HTTP."resources/themes/");
-define('PATH_CSS', PATH_HTTP."resources/css/");
-define('PATH_JS', PATH_HTTP."resources/js/");
-define('PATH_IMAGES', PATH_HTTP."resources/images/");
+define('PATH_THEMES', PATH_HTTP.PATH_RESOURCES."/themes/");
+define('PATH_CSS', PATH_HTTP.PATH_RESOURCES."/css/");
+define('PATH_JS', PATH_HTTP.PATH_RESOURCES."/js/");
+define('PATH_IMAGES', PATH_HTTP.PATH_RESOURCES."/images/");
 
+//Setting this to true triggers an attempt to log in to a CAS server using the following CAS configuration
 define('USECAS', false);
 
+define('CAS_URLS_BASE', NULL);
 define('CAS_LOGIN', "cas/login");
 define('CAS_CHECK', "cas/serviceValidate");
 define('CAS_LOGOUT', "cas/logout");
-define('CAS_URLS_BASE', NULL);
 define('CAS_URLS_LOGIN', CAS_URLS_BASE.CAS_LOGIN."?service=".PATH_HTTP."&renew=true");
 define('CAS_URLS_CHECK', CAS_URLS_BASE.CAS_CHECK."?service=".PATH_HTTP."&renew=true");
 define('CAS_URLS_LOGOUT', CAS_URLS_BASE.CAS_LOGOUT."?service=".PATH_HTTP."user.php?action=logout");
@@ -57,8 +67,10 @@ define('DB_PASSWORD', '');
 define('DB_HOST', '');
 define('DB_DATABASE', 'phpseedapp');
 define("DB_DSN", 'mysql:host='.DB_HOST.';dbname='.DB_DATABASE);
-// mssql has only been tested on Windows 
-//define("DB_PDO_DSN", 'sqlsrv:Server='.DB_HOST.';Database='.DB_DATABASE);
+
+//If switching to a non-MySQL flavor of SQL, expect to troubleshoot
+//define("DB_DSN", 'sqlsrv:Server='.DB_HOST.';Database='.DB_DATABASE);
+//define("DB_DSN", 'dblib:host='.DB_HOST.';dbname='.DB_DATABASE);
 
 //debug mode for PDO database queries
 define('DB_DEBUG', false);
@@ -78,8 +90,8 @@ $sitePages = array(
 //Set the log level: 0 = Everything, 3 = Errors only
 define("LOG_LEVEL",3);
 
-//The ViewRenderer can be set, here, if we want to use something other than the default HTMLViewRenderer
-//define('VIEW_RENDERER','BootstrapViewRenderer');
+//If VIEW_RENDERER is not set, the app will use the HTMLViewRenderer by default.
+define('VIEW_RENDERER','BootstrapViewRenderer');
 
 ?>
 
