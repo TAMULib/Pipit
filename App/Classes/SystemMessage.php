94347f9b3be56cffaddbc5b6d0405cd29132b4e2
jsavell <jsavell@library.tamu.edu>
2016-06-23 17:04:34 -0500

    there was nothing abstract about AbstractSystemMessage

--- App/Classes/SystemMessage.php
+++ App/Classes/SystemMessage.php
@@ -2,9 +2,10 @@
 namespace App\Classes;
 use Core\Classes as CoreClasses;
 /** 
-*	The default SystemMessage
+*	The app level SystemMessage.
+*
 *	@author Jason Savell <jsavell@library.tamu.edu>
 */
 
-class SystemMessage extends CoreClasses\AbstractSystemMessage {
+class SystemMessage extends CoreClasses\CoreSystemMessage {
 }
