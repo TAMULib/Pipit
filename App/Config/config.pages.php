40579e1bac7634e224e4031ce41c853d62590a54
jsavell <jsavell@library.tamu.edu>
2016-07-18 13:13:33 -0500

    remove legacy array from config.pages

--- App/Config/config.pages.php
+++ App/Config/config.pages.php
@@ -5,10 +5,6 @@ use App\Classes as AppClasses;
 //The keys correspond to controller names
 //Each entry should have a corresponding user reachable file (with an arbitrary real directory path) that includes the config file and (defines a controller and includes the loader) or (redirects with $forceRedirectUrl)
 //It's possible to have user reachable files that aren't represented in this array. They simply won't have a navigation link in the HTML header.
-/*$sitePages = array(
-			"widgets" => array("name"=>"widgets","path"=>"widgets"),
-			"users" => array("name"=>"users","path"=>"users","admin"=>true));
-*/
 $sitePages = array(
 			"widgets" => new AppClasses\SitePage("widgets","widgets",SECURITY_USER),
 			"users" => new AppClasses\SitePage("users","users",SECURITY_ADMIN));
