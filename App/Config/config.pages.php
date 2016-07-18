886fcda70e445ceea544859e9c93a5a9f43276f9
jsavell <jsavell@library.tamu.edu>
2016-07-18 13:20:29 -0500

    update pages config documentation

--- App/Config/config.pages.php
+++ App/Config/config.pages.php
@@ -1,10 +1,22 @@
 <?php
 namespace App\Config;
 use App\Classes as AppClasses;
-//This array represents the app's pages. Used to generate user facing navigation and load controllers
-//The keys correspond to controller names
-//Each entry should have a corresponding user reachable file (with an arbitrary real directory path) that includes the config file and (defines a controller and includes the loader) or (redirects with $forceRedirectUrl)
-//It's possible to have user reachable files that aren't represented in this array. They simply won't have a navigation link in the HTML header.
+/*
+The $sitePages array represents the app's pages, which are used to generate user facing navigation and load controllers.
+
+The keys correspond to controller names.
+
+Each entry should have a corresponding user reachable file (with an arbitrary relative directory path) that:
+	1. Includes the config file
+	2. Does one of the following:
+		a. Defines a controller and includes the loader 
+		b. Redirects with $forceRedirectUrl
+
+It's possible to have user reachable files that aren't represented in this array. They simply won't have a navigation link in the HTML header.
+
+Further configuration of the current SitePage is often done by the controllers that correspond to that SitePage.
+*/
+
 $sitePages = array(
 			"widgets" => new AppClasses\SitePage("widgets","widgets",SECURITY_USER),
 			"users" => new AppClasses\SitePage("users","users",SECURITY_ADMIN));
