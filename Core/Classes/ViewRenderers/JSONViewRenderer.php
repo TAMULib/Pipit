4777d78e3e31d3d39faa6451a11138f2c46def23
jsavell <jsavell@library.tamu.edu>
2017-10-06 14:21:14 -0500

    add some json_encode error handling

--- Core/Classes/ViewRenderers/JSONViewRenderer.php
+++ Core/Classes/ViewRenderers/JSONViewRenderer.php
@@ -14,12 +14,34 @@ class JSONViewRenderer implements Interfaces\ViewRenderer {
 	/** @var mixed[] $appContext An array of data for the views geared toward the app environment (User session, server paths, config)  */
 	private $appContext = null;
 
+	private static $encodeErrors = array(
+									JSON_ERROR_NONE => 'No error has occurred',
+									JSON_ERROR_DEPTH => 'The maximum stack depth has been exceeded',
+									JSON_ERROR_STATE_MISMATCH => 'Invalid or malformed JSON',
+									JSON_ERROR_CTRL_CHAR => 'Control character error, possibly incorrectly encoded',
+									JSON_ERROR_SYNTAX => 'Syntax error',
+									JSON_ERROR_UTF8 => 'Malformed UTF-8 characters, possibly incorrectly encoded',
+									JSON_ERROR_RECURSION => 'One or more recursive references in the value to be encoded',
+									JSON_ERROR_INF_OR_NAN => 'One or more NAN or INF values in the value to be encoded',
+									JSON_ERROR_UNSUPPORTED_TYPE => 'A value of a type that cannot be encoded was given',
+									JSON_ERROR_INVALID_PROPERTY_NAME => 'A property name that cannot be encoded was given',
+									JSON_ERROR_UTF16 => 'Malformed UTF-16 characters, possibly incorrectly encoded');
+
+	private function getEncodeErrors() {
+		return static::$encodeErrors;
+	}
+
 	/**
 	*	Render as JSON any registered view variables
 	*/
 	public function renderView() {
 		header('Content-Type: application/json;charset=utf-8');
-		echo json_encode($this->getViewVariables());
+		$json = json_encode($this->getViewVariables());
+		if ($json) {
+			echo $json;
+		} else {
+			throw new \RuntimeException($this->getEncodeErrors()[json_last_error()]);
+		}
 	}
 
 	/*
