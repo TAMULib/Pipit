93c1b377778cf36c56fbc8201e5e457041dcd1c8
Kevin Day <kday@tamu.edu>
2025-08-14 09:23:24 -0500

    Revert exception being thrown.
    The exceptions can be thrown in this class, but some object to explicit exceptions being thrown.
    Revert this change to favor those objections.

--- src/Pipit/Classes/Loaders/CoreLoader.php
+++ src/Pipit/Classes/Loaders/CoreLoader.php
@@ -218,7 +218,7 @@ class CoreLoader extends CoreObject implements Loader {
             if (!empty($path) && $this->isString($config, 'PATH_HTTP')) {
                 $this->getSite()->setRedirectUrl($config['PATH_HTTP']);
             } else {
-                throw new \RuntimeException($message);
+                exit;
             }
         }
     }
